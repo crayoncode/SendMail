6d24d675c744ffc32387ba504aad9c1b9e19799d
ZhaoYang <492118114@qq.com>
2016-05-31 09:55:19 +0800

    发送邮件

---
+++ ConsoleMail/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                       
+            EmailModel eModel = new EmailModel()
+            {
+                From = "492118114@qq.com",
+                Body = "<div style=\"background:red\"><h2>这是一个测试实例!  " + DateTime.Now + "</h2></div>",
+                DisplayName = "Jackie",
+                Host = "smtp.qq.com",
+                Subject = "测试",
+                To = "809968268@qq.com",
+                UserPwd = "",//发送邮箱的密码
+                UserID = "492118114@qq.com"
+            };
+            //发送邮件
+            new MailTool().Send(eModel);
+        }
+    }
+}
